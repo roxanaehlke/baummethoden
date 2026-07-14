b2909f00162c486406cf62694ea817ba1e5d8d0c
Roxana Ehlke <roxana.ehlke@gmail.com>
2026-07-14 11:48:40 +0200

    DIG-817 learn github actions

--- src/train.py
+++ src/train.py
@@ -6,4 +6,28 @@ import pickle
 # read csv-file
 data = pd.read_csv("data/mpg-data.csv", sep=";")
 
-print(data)
+# print(data)
+
+# shuffle data
+data = data.sample(frac=1)
+
+# 'class'-column
+y_variable = data["mpg"]
+
+# all columns that contain attributes
+x_variable = data.loc[:, data.columns != "mpg"]
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x_variable, y_variable, test_size=0.2
+)
+
+regressor = LinearRegression()
+
+regressor = regressor.fit(x_train, y_train)
+
+y_pred = regressor.predict(x_test)
+
+# print (y_pred)
+
+file_to_write = open("data/models/baummethoden_lr.pickle", "wb")
+pickle.dump(regressor, file_to_write)
